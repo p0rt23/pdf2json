f56e2cc67b7e161ada2ff278136eced2051924f9
Mark Smith <msmith4@gmail.com>
2023-08-20 21:59:23 +0000

    simple assertion

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
+import { strict as assert } from 'node:assert';
 import PDFParser from "pdf2json";
 
-// https://github.com/modesty/pdf2json/issues/76
-const pdfParser = new PDFParser(this, true);
+let pdfs = [
+  "sample.pdf"
+]
 
-pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
-pdfParser.on("pdfParser_dataReady", pdfData => {
-  // console.log(JSON.stringify(pdfData));
-  console.log(pdfParser.getRawTextContent());
+pdfs.forEach(filename => {
+  // https://www.npmjs.com/package/pdf2json
+  // https://github.com/modesty/pdf2json/issues/76
+  const pdfParser = new PDFParser(this, true);
+
+  const testPdf = (pages) => {
+    // console.log(pages[24]);
+    // paragraphs contain linebreaks that need to be removed for string matching
+    const page25 = pages[24].replace(/[\r\n]+/gm, " ");
+    assert.equal(
+      page25.includes("46% of firms in your group reported using a client segmentation strategy"),
+      true,
+      "Page 25 does not contain the client segmentation strategy."
+    );
+
+    console.log("All tests pass!");
+  }
+
+  const processPdf = (pdfData) => {
+    // pdfData contains metadata and all text objects
+    // console.log(JSON.stringify(pdfData));
+    const rawTextContent = pdfParser.getRawTextContent();
+
+    const pages = rawTextContent.split(/\-+Page \(\d+\) Break\-+/);
+    if (pages[pages.length - 1].trim() == "") {
+      pages.pop();
+    }
+
+    testPdf(pages);
+  }
+
+  pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
+  pdfParser.on("pdfParser_dataReady", processPdf);
+  pdfParser.loadPDF(filename);
 });
-
-pdfParser.loadPDF("sample.pdf");
--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 PROJECT="${PWD##*/}"
 
+docker image prune -f
 docker build . -t p0rt23/"$PROJECT":latest &&
 	docker run --rm --name "$PROJECT" p0rt23/"$PROJECT":latest
